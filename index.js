b09fe5ad4a2d78062ae2785238a7c2a73ea98cc0
kavenduro <plyron@mail.ru>
2022-01-03 19:46:18 +0300

    add clear callbaks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default class Store{
     static state = {};
-
+    static newCalKey = 0;
     static getState(key, dafault = undefined){
         if(Store.state[key] !== undefined && Store.state[key].val !== undefined){
             return Store.state[key].val;
@@ -11,29 +11,39 @@ export default class Store{
     }
     static runCallback(key){
         if(Store.state[key].callbacks !== undefined){
-            Store.state[key].callbacks.forEach(element => {
-                element(Store.state[key].val, Store.state[key].val);
-            });
+            for (let callback of Store.state[key].callbacks.values()){
+                callback({from: Store.state[key].val, to: Store.state[key].val});
+            }
         }
-    }     
+    }
     static addCallback(key, callback){
-        console.log('add-callback');
         if(Store.state[key] !== undefined){
             if(Store.state[key].callbacks === undefined){
-                Store.state[key].callbacks = [callback];
+                Store.state[key].callbacks = new Map();
             }
-            Store.state[key].callbacks.push(callback);
         }
         else{
-            Store.state[key] = { callbacks: [callback] };
+            Store.state[key] = { callbacks: new Map() };
         }
-    }       
+        let calkey = Store.newCalKey;
+        Store.newCalKey++;
+        Store.state[key].callbacks.set(calkey, callback);
+        return calkey;
+    }
+    static clearCallback(key, calkey){
+        if(Store.state[key] !== undefined){
+            if(Store.state[key].callbacks !== undefined){
+                Store.state[key].callbacks.delete(calkey);
+            }
+        }
+    }
     static setState(key, val){
         if(Store.state[key] !== undefined){
             if(Store.state[key].callbacks !== undefined){
-                Store.state[key].callbacks.forEach(element => {
-                    element({from: Store.state[key].val, to: val});
-                });
+                for (let callback of Store.state[key].callbacks.values()){
+                    callback({from: Store.state[key].val, to: val});
+                }
+
             }
             Store.state[key].val = val;
         }
@@ -42,3 +52,4 @@ export default class Store{
         }
     }
 }
+
